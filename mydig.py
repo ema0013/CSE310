4b9de0e6a82316be16a6091fd354af6548b04e20
ema0013 <ema0013@bths.edu>
2019-03-09 21:38:00 -0500

    changed some stuff in mydig

--- mydig.py
+++ mydig.py
@@ -1,4 +1,3 @@
-import calendar
 import time
 import datetime
 import dns.name
@@ -6,7 +5,7 @@ import dns.message
 import dns.query
 import dns.flags
 import sys
-
+#the 13 root servers, the program will test each one in case some don't work
 ROOT_SERVERS = ['198.41.0.4', '199.9.14.201', '192.33.4.12', '199.7.91.13', '192.203.230.10',
                 '192.5.5.241', '192.112.36.4', '198.97.190.53', '192.36.148.17', '192.58.128.30',
                 '193.0.14.129', '199.7.83.42', '202.12.27.33']
@@ -21,8 +20,8 @@ def dns_resolver(domain):
     for server in ROOT_SERVERS:
         try:
             print('Trying server', server)
-            query = dns.message.make_query(domain, 'A')
-            data = dns.query.udp(query, server, 5)
+            query = dns.message.make_query(domain, 'A') #query for answer
+            data = dns.query.udp(query, server, 5) #timeout is 5 seconds per root server
             if len(data.answer) == 0:
                 print('No response from server', server)
             else:
@@ -33,18 +32,18 @@ def dns_resolver(domain):
 
 
 if __name__ == '__main__':
-    dns_data = dns_resolver(sys.argv[1])
+    dns_data = dns_resolver(sys.argv[1]) #system arg so we can run from cmd
     print(';QUESTION SECTION:')
     for question in dns_data[0].question:
-       print(question)
+        print(question)
     print(';ANSWER SECTION:')
     for answer in dns_data[0].answer:
         print(answer)
-    print('Query Time: ', int(dns_data[1]), 'ms')
+    print('Query Time:', int(dns_data[1]), 'ms')
     print('WHEN:', datetime.datetime.now().strftime('%a'), datetime.datetime.now().strftime('%h'),
           datetime.datetime.now().strftime('%d'), datetime.datetime.now().strftime('%H:%M:%S'),
-          datetime.datetime.now().strftime('%Y'))
-    print('Message size rcvd:', len(dns_data[0].to_wire()))
+          datetime.datetime.now().strftime('%Y')) #current time
+    print('Message size rcvd:', len(dns_data[0].to_wire())) #towired length of the data
 
 
 
